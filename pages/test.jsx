c6e45ada3c9302a6a799cfd613b234d52337da03
Kay Kim <>
2020-11-23 14:06:15 +0900

    apply san-serif

--- pages/test.jsx
+++ pages/test.jsx
@@ -6,6 +6,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { Person, Search, Mic } from '@material-ui/icons';
 
 const Root = styled.div`
+  font-family: sans-serif;
   height: 100%;
   display: flex;
   flex-direction: column;
