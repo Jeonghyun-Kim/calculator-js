6c899ff6dc54ffe8b774cb6004763f50dc26734d
Kay Kim <>
2020-11-23 14:05:41 +0900

    add test  page

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
   body {
+    height: 100%;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+  #__next {
+    height: 100%;
   }
 `;
 
